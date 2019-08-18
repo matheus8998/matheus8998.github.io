d2f20bc4cdbba438f7c7d66674ab601426b1d73a
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2019-08-17 23:42:21 -0300

    Create quizes.js

---
+++ quizes.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("botao");
+const bear = document.getElementById("teste");
+
+button.addEventListener("click",buttone);
+
+function buttone() {
+bear.innerHTML = "urso";
+}
