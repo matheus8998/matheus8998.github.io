7d62f67bc3fd5adf91d965fbdd55c3a71dd8ca1d
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2019-08-17 23:50:02 -0300

    Delete quizes.js

--- quizes.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById("botao");
-const bear = document.getElementById("teste");
-
-button.addEventListener("click",buttone);
-
-function buttone() {
-alert('erro');
-}
