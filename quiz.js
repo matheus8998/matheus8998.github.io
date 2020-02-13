64db283c3d417393b2902366db9c35d877f11004
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2020-02-13 02:41:31 -0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,7 +1,14 @@
-const notas = document.getElementById("notas");
-const botao = document.getElementById("botao");
 
-cifraAleatoria() {
+
+function cifraAleatoria() {
     alert("ALERTA");
-    botao.style.display = "none";
+    document.getElementById("botao").style.display = "none";
+
+    var notasNaturais = ["A", "B", "C", "D", "E", "F", "G"]
+
+    for (notasNaturais, i = notasNaturais.length; i--; ) {
+        var notasAleatorias = notasNaturais.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+        document.getElementById("divNotas").innerHTML +=  '<span>'+notasAleatorias+'</span>';
+    }
 }
+
