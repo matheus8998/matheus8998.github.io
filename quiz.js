1ecbea6e0c6be21ca03dc8a567efab9cd1b999ea
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2020-02-14 01:16:25 -0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,14 +1,17 @@
 
 
 function cifraAleatoria() {
-    alert("ALERTA");
-    document.getElementById("botao").style.display = "none";
+  //  document.getElementById("botao").style.display = "none";
 
     var notasNaturais = ["A", "B", "C", "D", "E", "F", "G"]
 
+    var notasAleatorias = '';
+
+    document.getElementById("divNotas").innerHTML = '';
+
     for (notasNaturais, i = notasNaturais.length; i--; ) {
-        var notasAleatorias = notasNaturais.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
-        document.getElementById("divNotas").innerHTML +=  '<span>'+notasAleatorias+'</span>';
+         notasAleatorias = notasNaturais.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+        document.getElementById("divNotas").innerHTML +=  '<span> '+notasAleatorias+' </span>';
     }
 }
 
