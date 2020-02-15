4578d24bc391415972b2a12d19f8ec80dd122792
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2020-02-15 14:48:02 -0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,8 +2,17 @@
 
 function cifraAleatoria() {
   //  document.getElementById("botao").style.display = "none";
+    var dificuldade = document.getElementById("dificuldade");
+    
+    var dificuldadeSelecionada = dificuldade.options[dificuldade.selectedIndex].value;
 
-    var notasNaturais = ["A", "B", "C", "D", "E", "F", "G"]
+
+    if (dificuldadeSelecionada == "naturais") {   
+            var notasNaturais = ["A", "B", "C", "D", "E", "F", "G"];
+}
+    else  {  
+            var notasNaturais = ["A","A#", "B", "C", "C#", "D","D#", "E", "F","F#", "G", "G#"];
+}
 
     var numeroCorda = Math.floor(Math.random() * (4 - 1 + 1) + 1);
 
@@ -13,7 +22,7 @@ function cifraAleatoria() {
 
     document.getElementById("numero-corda").innerHTML = '';
 
-    document.getElementById("numero-corda").innerHTML += '<p>' +numeroCorda +'ª corda </p>';
+    document.getElementById("numero-corda").innerHTML += '<p id="corda">' +numeroCorda +'ª corda </p>';
 
     for (notasNaturais, i = notasNaturais.length; i--; ) {
          notasAleatorias = notasNaturais.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
