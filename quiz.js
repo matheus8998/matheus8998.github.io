dcedc9107e0ec75719b66391bd86b81b71222524
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2020-02-15 01:16:17 -0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -5,9 +5,15 @@ function cifraAleatoria() {
 
     var notasNaturais = ["A", "B", "C", "D", "E", "F", "G"]
 
-    var notasAleatorias = '';
+    var numeroCorda = Math.floor(Math.random() * (4 - 1 + 1) + 1);
+
+ //   var notasAleatorias = '';
 
     document.getElementById("divNotas").innerHTML = '';
+
+    document.getElementById("numero-corda").innerHTML = '';
+
+    document.getElementById("numero-corda").innerHTML += '<p>' +numeroCorda +'ª corda </p>';
 
     for (notasNaturais, i = notasNaturais.length; i--; ) {
          notasAleatorias = notasNaturais.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
